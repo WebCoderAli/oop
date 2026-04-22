93859207a0019118f695f2af9f630794e038a74a
Ali Hamza <devwithali@gmail.com>
2026-04-22 17:04:15 +0500

    Enhance Teacher class with salary management
    Added private salary property and corresponding setter/getter methods in Teacher class.

--- class.cpp
+++ class.cpp
@@ -3,28 +3,33 @@
 using namespace std;
 
 class Teacher{
+	//private modifier
 	private:
+	//private properties
 		float salary;
-	
+	//public modifier
 	public:
+	//public properties
 		string name;
 		string subject;
 		string dept;
-		// setter 
+		// setter / public methods
 		void changeSalary(float s){
 			salary = s;
 		}
-		
+		// getter / public methods
 		float showSalary(){
 			return salary;
 		}
 };
 
 int main(){
+	//creating object t1 from Teacher class and assgning values
 	Teacher t1;
 	t1.name = "Ali Hamza";
 	t1.subject = "Maths";
 	t1.dept = "Computer Science";
+	//use setter to access private modifier
 	t1.changeSalary(25000);
 	
 	cout<<t1.name<<endl;
